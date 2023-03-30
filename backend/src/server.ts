fdb795a00d2ed09f25a85b8081480a18558b1750
taldoflemis <coderflemis@gmail.com>
2023-03-30 18:29:13 -0300

    atualizar usuarios para conter numbero e idade

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,16 @@ import { config } from "dotenv";
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser"
 import marcelo from "./usuarios";
+import cors from "cors"
 
 config();
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -18,17 +24,20 @@ app.post("/criar", (req: Request, res: Response) => {
   const nome = body?.nome
   const idade = body?.idade
   const email = body?.email
+  const number = body?.number
 
-  if (!nome || !idade || !email) {
+  if (!nome || !idade || !email || !number) {
     res.status(400).send("Dados inválidos")
   }
 
-  res.status(201).send(`Meu nome é ${nome} e tenho ${idade} anos e meu email é ${email}`);
+  res.status(201).send(`Meu nome é ${nome} e tenho ${idade} anos e meu email é ${email} e meu número é ${number}`);
 });
+
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
+
 
 process.on("SIGTERM", () => {
   console.info("SIGTERM signal received.");
--- backend/src/usuarios.ts
+++ backend/src/usuarios.ts
@@ -1,24 +1,28 @@
 export interface Usuario {
-    nome: string;
-    idade: number;
+    name: string;
+    number: number;
     email: string;
+    address: string;
 }
 
 const usuarios: Usuario[] = [
     {
-        nome: "kanye west",
-        idade: 762,
+        name: "kanye west",
+        number: 762,
         email: "fuzildeassalto.com.br",
+        address: "Exquina da Ixcola",
     },
     {
-        nome: "drake",
-        idade: 12,
+        name: "drake",
+        number: 12,
         email: "drake.com.br",
+        address: "okalekdo"
     },
     {
-        nome: "JONAH HILL",
-        idade: 157,
+        name: "JONAH HILL",
+        number: 157,
         email: "bungeejump@apartamento.com.br",
+        address: "wepowkdpkpw"
     },
 ]
 
